0f7c53a801ad9365fe49385c74cdd4a6eb558080
AnantaAkashPodder <anantaakash.podder@gmail.com>
2024-01-21 12:20:15 +0600

    - Added "Spring Data JPA & Spring JDBC" dependency & FootballPlayerEntity class created

---
+++ application/e_learniverse_spring_boot/src/main/java/com/akash/e_learniverse_spring_boot/entity/FootballPlayerEntity.java
@@ -0,0 +1,35 @@
+package com.akash.e_learniverse_spring_boot.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "football_player")
+public class FootballPlayerEntity {
+
+    @Id //Sequence mane hocche, amader ID field Auto increment hobe... ei author_id_seq amra DBVear er Sequence eo pabo
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "football_player_id_seq")
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private Integer age;
+
+    @JsonProperty("jersey_no") //Json format ee jerseyNumber takbe "jersey_no" hisave
+    private Integer jerseyNumber;
+}
