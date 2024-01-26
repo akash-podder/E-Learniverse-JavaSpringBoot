68836e4b5ab997420fd140580e2b10de8e34821c
AnantaAkashPodder <anantaakash.podder@gmail.com>
2024-01-26 16:26:04 +0600

    - `AdminRestController` added

---
+++ application/e_learniverse_spring_boot/src/main/java/com/akash/e_learniverse_spring_boot/controller/rest_controller/AdminRestController.java
@@ -0,0 +1,36 @@
+package com.akash.e_learniverse_spring_boot.controller.rest_controller;
+
+import com.akash.e_learniverse_spring_boot.entity.FootballPlayerEntity;
+import com.akash.e_learniverse_spring_boot.response.ApiResponseDto;
+import com.akash.e_learniverse_spring_boot.service.FootballPlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("admin/")
+public class AdminRestController {
+    private static final Logger logger = LogManager.getLogger(AdminRestController.class);
+
+    private final FootballPlayerService footballPlayerService;
+
+
+    @Autowired
+    public AdminRestController(FootballPlayerService footballPlayerService) {
+        this.footballPlayerService = footballPlayerService;
+    }
+
+    @GetMapping("player-all")
+    public ResponseEntity<?> getAllPlayer() {
+        List<FootballPlayerEntity> playerEntityList = footballPlayerService.getAllFootballPlayer();
+
+        return ResponseEntity.ok(new ApiResponseDto("Player List: " + playerEntityList.toString()));
+    }
+}
